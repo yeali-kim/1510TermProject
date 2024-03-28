cf22a9f77f7203cb94442aca7da87fad777b8434
Seogin Hong <hsihsj94@gmail.com>
2024-03-28 16:23:33 -0700

    Reformat npc.py

--- npc.py
+++ npc.py
@@ -4,21 +4,24 @@ def jinkx():
         "yes_response": "I knew it! Let's begin. Avada Kedavra!",
         "no_response": "Fine. I didn't see a great wizard in you anyways."
     }
-    
+
+
 def chrissipus():
     responses = {
         "greeting": "Hello there, tiny being! I can teach you some sword fighting skills. Would you like to learn some? (Y/N)",
         "yes_response": "I knew it! Grab your sword my friend! Not that one. That one is expensive.",
         "no_response": "Fine. You don't look that strong anyways."
     }
-    
+
+
 def hypatia():
     responses = {
         "greeting": "Greetings, traveler! I can teach you some archery skills. Would you like to learn some? (Y/N)",
         "yes_response": "I knew it! I will make you the best archer in the world.",
         "no_response": "Fine. You look clumsy anyways. Archery is not for everyone."
     }
-    
+
+
 def shawn():
     responses = {
         "greeting": "My daughter Heca has been missing for a month. Can you help me find her? (Y/N)",
@@ -27,7 +30,8 @@ def shawn():
         "fulfilled": "Thank you so much for finding my daughter! Here is a reward for you.",
         "unfulfilled": "I'm still waiting for you to find my daughter. Please hurry."
     }
-    
+
+
 def david():
     responses = {
         "greeting": "I need some tree branches to make chopsticks with. Can you help me? (Y/N)",
@@ -36,7 +40,8 @@ def david():
         "fulfilled": "Thank you so much! Here is your reward.",
         "unfulfilled": "I'm still waiting for those branches. Please hurry."
     }
-    
+
+
 def daniel():
     responses = {
         "greeting": "Welcome to my shop! Full health elixirs are 50G each. Would you like to buy one? (Y/N)",
@@ -45,10 +50,11 @@ def daniel():
         "fulfilled": "Thank you for your purchase! Use your elixir wisely.",
         "unfulfilled": "You don't have enough gold to buy a potion. Come back when you do."
     }
-    
+
+
 def daughter():
     responses = {
         "greeting": "Thank you for finding me! I was lost in the forest. My father must be worried sick."
     }
-    
-def dragon():
+
+# def dragon():
