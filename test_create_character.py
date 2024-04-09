2ee3181eaf7cd796bad100c006bfbb9a3a39f25a
YEALI KIM <YKIM229@MY.BCIT.CA>
2024-04-08 17:42:02 -0700

    Write unit test for create_character

---
+++ test_create_character.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from character_functions import create_character
+from unittest.mock import patch
+
+
+class Test(TestCase):
+    @patch('character_functions.random.randint')
+    def test_create_character(self, mock_randint):
+        mock_randint.side_effect = [5, 5, 5]
+        expected = character = {'class': 'Citizen', 'stats': [5, 5, 5],
+                                'location': {'x-coordinate': 6, 'y-coordinate': 2}, 'level': 1, 'exp': 0,
+                                'skills': {'Tackle': 'normal'}, 'hp': 100, 'max_hp': 100, 'elixir': 1, 'gold': 0,
+                                'shawn_quest': None, 'david_quest': None, 'heca_found': False,
+                                'tree_branches': 0, 'chris': False}
+        actual = create_character()
+        self.assertEqual(expected, actual)
+
