6103e0ad3384c682a26f6a082486fa97f39d01d8
YEALI KIM <YKIM229@MY.BCIT.CA>
2024-04-10 12:42:43 -0700

    Make unit test files

---
+++ test_calculate_skill_damage.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_calculate_skill_damage(self):
+        self.fail()
--- test_create_character.py
+++ test_create_character.py
@@ -7,11 +7,10 @@ class TestCreateCharacter(TestCase):
     @patch('character_functions.random.randint')
     def test_create_character(self, mock_randint):
         mock_randint.side_effect = [5, 5, 5]
-        expected = character = {'class': 'Citizen', 'stats': [5, 5, 5],
-                                'location': {'x-coordinate': 6, 'y-coordinate': 2}, 'level': 1, 'exp': 0,
-                                'skills': {'Tackle': 'normal'}, 'hp': 100, 'max_hp': 100, 'elixir': 1, 'gold': 0,
-                                'shawn_quest': None, 'david_quest': None, 'heca_found': False,
-                                'tree_branches': 0, 'chris': False}
+        expected = {'class': 'Citizen', 'stats': [5, 5, 5], 'location': {'x-coordinate': 6, 'y-coordinate': 2},
+                    'level': 1, 'exp': 0, 'skills': {'Tackle': 'normal'}, 'hp': 100, 'max_hp': 100, 'elixir': 1,
+                    'gold': 0, 'shawn_quest': None, 'david_quest': None, 'heca_found': False, 'tree_branches': 0,
+                    'chris': False}
         actual = create_character()
         self.assertEqual(expected, actual)
 
