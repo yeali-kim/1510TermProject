d7cc23df36f5e6b03a2b7924e8595559b39ad9c5
YEALI KIM <YKIM229@MY.BCIT.CA>
2024-04-10 19:08:36 -0700

    Add unit test for print for update_level

--- test_update_level.py
+++ test_update_level.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from character_functions import update_level
+from unittest.mock import patch
+from io import StringIO
 
 
 class TestUpdateLevel(TestCase):
@@ -24,5 +26,24 @@ class TestUpdateLevel(TestCase):
                     'max_hp': 530,
                     'stats': [10, 11, 55]}
         self.assertEqual(expected, player)
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_update_level_level_no_up_print(self, mock_stdout):
+        player = {"class": "Citizen", "level": 1, "exp": 90, "stats": [1, 1, 1], "max_hp": 100}
+        update_level(player)
+        expected = "You need 10 more Exp to reach level 2.\n"
+        self.assertEqual(expected, mock_stdout.getvalue())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_update_level_level_up_print(self,mock_stdout):
+        player = {"class": "Citizen", "level": 1, "exp": 100, "stats": [1, 1, 1], "max_hp": 100}
+        update_level(player)
+        expected = ("Congratulations! Your character is now level 2.\n"
+                    "Your stats have increased:\n"
+                    "Strength: 2, Dexterity: 2, Intelligence: 2, HP: 120\n"
+                    "You need 101 more Exp to reach level 3.\n")
+        self.assertEqual(expected, mock_stdout.getvalue())
+
+
 
         
