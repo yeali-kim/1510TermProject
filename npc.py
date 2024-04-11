8b4d1898ff454ff8d86d63c24d9e4497eeafd8cf
Seogin Hong <hsihsj94@gmail.com>
2024-04-10 23:50:57 -0700

    Finish docstring for all functions

--- npc.py
+++ npc.py
@@ -5,6 +5,18 @@ import combat
 
 
 def jinkx(character: dict[str, str | int | bool | dict[str, int]]):
+    """
+    Offers the player's character an opportunity to learn magic and become a 'Magician'
+    if they are currently a 'Citizen'.
+
+    :param character: a dictionary representing a character
+    :precondition: the character dictionary must contain a class key
+    :precondition: the character's class must be Citizen for the class change to occur
+    :postcondition: if the character is a Citizen and the player accepts Jinkx's offer, the character's class is
+    changed to Magician and their skills are updated accordingly
+    :postcondition: if the character is a Knight or an Archer, Jinkx responds differently, and no class change occurs
+    :postcondition: if the character is a Citizen but the player refuses Jinkx's offer, no class change occurs
+    """
     if character['class'] == 'Citizen':  # check if class is Citizen
         # ask user if they want to talk to Jinkx
         say_hi = input(
@@ -50,6 +62,19 @@ def jinkx(character: dict[str, str | int | bool | dict[str, int]]):
 
 
 def chrissipus(character: dict[str, str | int | bool | dict[str, int]]):
+    """
+    Offers the player's character an opportunity to learn chivalry and become a 'Knight'
+    if they are currently a 'Citizen'.
+
+    :param character: a dictionary representing a character
+    :precondition: the character dictionary must contain a class key
+    :precondition: the character's class must be Citizen for the class change to occur
+    :postcondition: if the character is a Citizen and the player accepts chrissipus' offer, the character's class is
+    changed to Knight and their skills are updated accordingly
+    :postcondition: if the character is a Magician or an Archer, chrissipus responds differently,
+    and no class change occurs
+    :postcondition: if the character is a Citizen but the player refuses chrissipus' offer, no class change occurs
+    """
     if character['class'] == 'Citizen':  # check if class is Citizen
         say_hi = input(
             "You met \033[1;35mChrissipus\033[0m, the mighty knight. Do you want to talk to him? (Y/N)").strip().lower()
@@ -97,6 +122,18 @@ def chrissipus(character: dict[str, str | int | bool | dict[str, int]]):
 
 
 def hypatia(character: dict[str, str | int | bool | dict[str, int]]):
+    """
+    Offers the player's character an opportunity to learn detecting and become a 'Archer'
+    if they are currently a 'Citizen'.
+
+    :param character: a dictionary representing a character
+    :precondition: the character dictionary must contain a class key
+    :precondition: the character's class must be Citizen for the class change to occur
+    :postcondition: if the character is a Citizen and the player accepts hypatia's offer, the character's class is
+    changed to Archer and their skills are updated accordingly
+    :postcondition: if the character is a Magician or a Knight, hypatia responds differently and no class change occurs
+    :postcondition: if the character is a Citizen but the player refuses hypatia's offer, no class change occurs
+    """
     if character['class'] == 'Citizen':  # check if class is Citizen
         say_hi = input(
             "You met \033[1;35mHypatia\033[0m, the great archer. Do you want to talk to her? (Y/N)").strip().lower()
@@ -142,6 +179,18 @@ def hypatia(character: dict[str, str | int | bool | dict[str, int]]):
 
 
 def shawn(character: dict[str, str | int | bool | dict[str, int]]):
+    """
+    Initiates or updates the status of Shawn's quest based on the character's interaction and quest progress.
+
+    :param character: a dictionary representing a character
+    :precondition: the character dictionary must contain shawn_quest to indicate if shawn's quest has been accepted
+    and heca_found to indicate if haca has been found
+    :postcondition: if the user accepts shawn's quest, shawn_quest is set to True
+    :postcondition: if the user has completed the quest by finding heca, shawn_quest is se to False,
+    and the character is rewarded with elixirs
+    :postcondition: function updates the character's state based on the quest's progress and the user's action
+    during the encounter
+    """
     if character["shawn_quest"] is None:
         say_hi = input(
             "You met \u001b[34;1mShawn\033[0m, the mayor. Do you want to talk to him? (Y/N): ").strip().lower()
@@ -191,6 +240,16 @@ def shawn(character: dict[str, str | int | bool | dict[str, int]]):
 
 
 def heca(character: dict[str, str | int | bool | dict[str, int]]):
+    """
+    Updates the quest status related to finding Heca, based on the character's current quest progress.
+
+    :param character: a dictionary representing a character
+    :precondition: character must contain shawn_quest and heca_found
+    :postcondition: if shawn_quest is True and heca_found is False, the function updates heca_found to True,
+    and print messages to the user
+    :postcondition: if heca_found is already True, a brief message is printed
+    :postcondition: if shawn_quest is False or not present, a message indicates that the character is unrecognized
+    """
     if character["shawn_quest"] and character["heca_found"] is False:
         heca_lines = [
             "\u001b[36mHeca\033[0m: Yes, Shawn is my father! Thank you so much for finding me!",
@@ -208,6 +267,16 @@ def heca(character: dict[str, str | int | bool | dict[str, int]]):
 
 
 def heca_found(character: dict[str, str | int | bool | dict[str, int]], board: dict) -> dict[tuple[int, int], str]:
+    """
+    Updates the game board to change heca's location to Desert once she has been found.
+
+    :param character: a dictionary representing a character
+    :param board: a dictionary mapping (x, y) coordinated tuples to strings describing the area
+    :precondition: character must contain shawn_quest and heca_found keys
+    :precondition: board must accurately represent the game's areas and include heca
+    :postcondition: if the specified conditions are met, heca's location on the board is updated to Desert
+    :return: the updated board dictionary with heca's location changed to Desert if the conditions are met.
+    """
     if character["shawn_quest"] is False and character["heca_found"]:
         heca_coord = (key for key, val in board.items() if val == "heca")
         board[heca_coord] = "Desert"
@@ -215,6 +284,16 @@ def heca_found(character: dict[str, str | int | bool | dict[str, int]], board: d
 
 
 def david(character: dict[str, str | int | bool | dict[str, int]]):
+    """
+    Manages a quest given by david, asking for help to collect tree branches.
+
+    :param character: a dictionary representing a character
+    :precondition: character must have keys for david_quest, tree_branches, and gold
+    :postcondition: if the quest is accepted, david_quest is set to True
+    :postcondition: if character has tree branches more than 10, david_quest is set to False,
+    tree_branches is decreased by 10, and the character receives a gold reward
+    :postcondition: if complete the quest, print a final message from david
+    """
     if character["david_quest"] is None:
         say_hi = input(
             "You met \u001b[34;1mDavid\033[0m, the carpenter. Do you want to talk to him? (Y/N): ").strip().lower()
@@ -267,6 +346,14 @@ def david(character: dict[str, str | int | bool | dict[str, int]]):
 
 
 def get_valid_elixir_quantity(prompt: str):
+    """
+    Prompts the user for an integer quantity of elixirs, ensuring the input is valid and greater than 0.
+
+    :param prompt: a string containing the message to display to the user when asking for input
+    :precondition: the input for quantity should be an integer
+    :postcondition: return an integer representing a valid quantity of elixirs as input by the user
+    :return: an integer representing the quantity of elixirs
+    """
     while True:
         try:
             quantity = int(input(prompt))
@@ -279,6 +366,17 @@ def get_valid_elixir_quantity(prompt: str):
 
 
 def daniel(character: dict[str, str | int | bool | dict[str, int]]):
+    """
+    Facilitates an interaction between the character and daniel for purchasing elixirs.
+
+    :param character: a dictionary representing a character
+    :precondition: character must contain gold and elixir keys
+    :postcondition: if the player decides to buy elixirs and has enough gold, gold is decreased by the
+    total purchase cost, and elixir is increased by the quantity bought
+    :postcondition: print message if user try to buy more elixirs than their gold allows
+    :postcondition: print feedback on the transaction outcome, including any adjustments to the character's gold
+    and elixir count
+    """
     say_hi = input(
         "You met \u001b[34;1mDaniel\033[0m, the apothecary. Do you want to talk to him? (Y/N): ").strip().lower()
     while say_hi not in ["y", "n"]:
@@ -316,6 +414,14 @@ def daniel(character: dict[str, str | int | bool | dict[str, int]]):
 
 
 def ending(character: dict[str, str | int | bool | dict[str, int]]):
+    """
+    Unveils the game's ending narrative, reflecting on the character's impact on the world
+    and presenting a final choice.
+
+    :param character: a dictionary representing a character
+    :precondition: character must contain class and name
+    :postcondition: based on the user's choice, character is updated with a new class and their skills are refreshed
+    """
     scripts = [
         "Chris (with his last breath): See the world through my eyes, and understand the truth you refused to see.",
         "The player returns to the villages and landscapes they once 'saved', only to find them in ruins or eerily "
@@ -347,6 +453,14 @@ def ending(character: dict[str, str | int | bool | dict[str, int]]):
 
 
 def skill_fire_breath(character: dict[str, str | int | bool | dict[str, int]]):
+    """
+    Simulates a dodging attempt against a dragon's fire breath attack in a combat scenario.
+
+    :param character: a dictionary representing a character
+    :precondition: character must contain a hp key
+    :postcondition: if the user fails to dodge the dragon's fire breath, hp is decreased by 700
+    :postcondition: if the user successfully dodges the attack, the character's hp remains unchanged
+    """
     print("Danger looms! Chris inhales deeply, preparing to unleash his dragon breath!!")
     user_choice = input("Quick! Enter 'up', 'down', 'left', or 'right' to dodge the inferno!!")
     user_choice = user_choice.lower()
@@ -360,6 +474,15 @@ def skill_fire_breath(character: dict[str, str | int | bool | dict[str, int]]):
 
 
 def skill_question(character: dict[str, str | int | bool | dict[str, int]]):
+    """
+    Challenges the character with a memory test, impacting their maximum health based on their response.
+
+    :param character: a dictionary representing a character
+    :precondition: character must contain a max_hp key
+    :postcondition: if the user successfully recalls and inputs the correct word,
+    the character's state remains unchanged
+    :postcondition: if the user fails to recall the word, the character's max_hp is reduced by 100
+    """
     print("Chris eyes you with a cunning glint. 'Let's test your agility,' he smirks.")
     print("'Catch a glimpse of this word; it will vanish in 0.3 seconds. Be ready...'")
     print("Open your eyes...")
@@ -379,6 +502,19 @@ def skill_question(character: dict[str, str | int | bool | dict[str, int]]):
 
 
 def battle_chris(character: dict[str, str | int | bool | dict[str, int]], dragon_chris: dict[str, int | str]):
+    """
+    Engages the player's character in a combat encounter with Dragon Chris, featuring skill selection,
+    damage calculation, and dynamic reactions from Chris.
+
+    :param character: a dictionary representing a character
+    :param dragon_chris: a dictionary representing dragon chris
+    :precondition: character must contain a hp key
+    :precondition: dragon_chris must contain health and type keys
+    :postcondition: the character's hp may be reduced based on the outcome of dragon chris
+    :postcondition: dragon chris's health is reduced based on the damage dealt by character
+    :postcondition: if dragon chris's health drops to or below 0, the battle is won, and the function terminates
+    :postcondition: if the character's hp drops to or below 0, it results in a game over
+    """
     chosen_skill, chosen_type = combat.choose_skill(character)
     damage_dealt = combat.calculate_skill_damage(chosen_skill, chosen_type, character, dragon_chris)
     print(f"Unleashing {chosen_skill}, you inflict {damage_dealt} damage upon Chris.")
@@ -405,6 +541,13 @@ def battle_chris(character: dict[str, str | int | bool | dict[str, int]], dragon
 
 
 def chris(character: dict[str, str | int | bool | dict[str, int]]):
+    """
+    Initiates the final encounter with Chris, the dragon, blending narrative elements with combat mechanics.
+
+    :param character: a dictionary representing a character
+    :precondition: character must contain name and location
+    :postcondition: if user won, character's location is updated to home
+    """
     dragon_chris = {'health': 5000, 'type': random.choice(["fire", "water", "grass", "normal"])}
     print("You've encountered the dragon Chris!!")
     print(f"Chris has {dragon_chris['health']} hp and type is {dragon_chris['type']}")
