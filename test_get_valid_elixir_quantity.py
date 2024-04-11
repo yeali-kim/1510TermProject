c0f7d0216b6701c07d01476bf190e283089d856c
YEALI KIM <YKIM229@MY.BCIT.CA>
2024-04-10 20:33:42 -0700

    Write unit test for get_valid_elixir_quantity

---
+++ test_get_valid_elixir_quantity.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from unittest.mock import patch
+from io import StringIO
+from npc import get_valid_elixir_quantity
+
+
+class TestGetValidElixirQuantity(TestCase):
+    @patch('sys.stdout', new_callable=StringIO)
+    @patch('builtins.input', return_value="one")
+    def test_get_valid_elixir_quantity_invalid_print(self, _, mock_output):
+        get_valid_elixir_quantity('How many do you want to buy?:')
+        expected = "You need to enter a valid integer greater than 0.\n"
+        self.assertEqual(expected, mock_output.getvalue())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    @patch('builtins.input', return_value='')
+    def test_get_valid_elixir_quantity_empty(self, _, mock_output):
+        get_valid_elixir_quantity('How many do you want to buy?:')
+        expected = "You need to enter a valid integer greater than 0.\n"
+        self.assertEqual(expected, mock_output.getvalue())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    @patch('builtins.input', return_value='-1')
+    def test_get_valid_elixir_quantity_negative(self, _, mock_output):
+        get_valid_elixir_quantity('How many do you want to buy?:')
+        expected = "Quantity must be an integer greater than 0.\n"
+        self.assertEqual(expected, mock_output.getvalue())
+
+    @patch('builtins.input', return_value='1')
+    def test_get_valid_elixir_valid(self, _):
+        actual = get_valid_elixir_quantity('How many do you want to buy?:')
+        expected = 1
+        self.assertEqual(expected, actual)
