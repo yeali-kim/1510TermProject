9e6d6b74b3c90549fea9b42c625a0d3f6ffccaf1
YEALI KIM <YKIM229@MY.BCIT.CA>
2024-04-11 12:26:07 -0700

    Fix infinite loop in unit test for get_valid_elixir_quantity

--- test/test_get_valid_elixir_quantity.py
+++ test/test_get_valid_elixir_quantity.py
@@ -6,21 +6,21 @@ from npc import get_valid_elixir_quantity
 
 class TestGetValidElixirQuantity(TestCase):
     @patch('sys.stdout', new_callable=StringIO)
-    @patch('builtins.input', return_value="one")
+    @patch('builtins.input', side_effect=['one', 1])
     def test_get_valid_elixir_quantity_invalid_print(self, _, mock_output):
         get_valid_elixir_quantity('How many do you want to buy?:')
         expected = "You need to enter a valid integer greater than 0.\n"
         self.assertEqual(expected, mock_output.getvalue())
 
     @patch('sys.stdout', new_callable=StringIO)
-    @patch('builtins.input', return_value='')
+    @patch('builtins.input', side_effect=['', 1])
     def test_get_valid_elixir_quantity_empty(self, _, mock_output):
         get_valid_elixir_quantity('How many do you want to buy?:')
         expected = "You need to enter a valid integer greater than 0.\n"
         self.assertEqual(expected, mock_output.getvalue())
 
     @patch('sys.stdout', new_callable=StringIO)
-    @patch('builtins.input', return_value='-1')
+    @patch('builtins.input', side_effect=['-1', 1])
     def test_get_valid_elixir_quantity_negative(self, _, mock_output):
         get_valid_elixir_quantity('How many do you want to buy?:')
         expected = "Quantity must be an integer greater than 0.\n"
