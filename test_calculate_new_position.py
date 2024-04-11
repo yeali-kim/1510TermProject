ec0badd9c3be5fad45b8d4762045c39b50a9da77
YEALI KIM <YKIM229@MY.BCIT.CA>
2024-04-10 17:48:01 -0700

    Write unit test for calculate_new_position

--- test_calculate_new_position.py
+++ test_calculate_new_position.py
@@ -3,5 +3,20 @@ from character_functions import calculate_new_position
 
 
 class TestCalculateNewPosition(TestCase):
-    def test_calculate_new_position(self):
-        self.fail()
+    def test_calculate_new_position_up(self):
+        x = 1
+        y = 1
+        direction = "up"
+        movement = {"up": (0, -1), "down": (0, 1), "left": (-1, 0), "right": (1, 0)}
+        result = calculate_new_position(x, y, direction, movement)
+        expected = (1, 0)
+        self.assertEqual(expected, result)
+
+    def test_calculate_new_position_left(self):
+        x = 1
+        y = 1
+        direction = "left"
+        movement = {"up": (0, -1), "down": (0, 1), "left": (-1, 0), "right": (1, 0)}
+        result = calculate_new_position(x, y, direction, movement)
+        expected = (0, 1)
+        self.assertEqual(expected, result)
