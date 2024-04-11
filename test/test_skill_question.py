c085745d16fddb65617422dfc351d40fe313811e
YEALI KIM <YKIM229@MY.BCIT.CA>
2024-04-11 14:53:14 -0700

    Write unit test for skill_question

--- test/test_skill_question.py
+++ test/test_skill_question.py
@@ -4,12 +4,8 @@ from npc import skill_question
 
 
 class TestSkillQuestion(TestCase):
-    @patch('npc.random.choice')
-    @patch('builtins.input')
-    def test_skill_question_right(self, mock_input, mock_choice):
-        character = {"max_hp": 300}
-        skill_question(character)
-        mock_choice.return_value = "rat"
-        mock_input.return_value = "rat"
-        expected = {"max_hp": 200}
-        self.assertEqual(expected, character)
+    @patch('random.choice', return_value='rat')
+    def test_skill_question(self, _):
+        actual = skill_question()
+        expected = 'rat'
+        self.assertEqual(expected, actual)
